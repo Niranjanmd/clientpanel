2b44ab6bdc3d64f0026d90249df01406b7a01aae
Niranjan MD <Niranjanmd09@gmail.com>
2017-12-01 09:04:37 +0530

    register new user

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -23,4 +23,12 @@ export class AuthService {
   logout() {
     this.afAuth.auth.signOut();
   }
+
+  register(email: string, password: string) {
+    return new Promise((resolve, reject) => {
+      this.afAuth.auth.createUserWithEmailAndPassword(email, password)
+      .then(userdata => resolve(userdata),
+      err => reject(err));
+    });
+  }
 }
